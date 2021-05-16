3bb749705f56c69a8e601cd53bc1db660693725c
gimse <23360355+gimse@users.noreply.github.com>
2021-05-16 20:38:26 +0200

    Adding logic for missing key and returning image.

--- index.php
+++ index.php
@@ -3,11 +3,31 @@ require "./vendor/autoload.php";
 use masokky\QuoteMaker;
 try{
   $text = "the cruelest crime is giving false hope without love";
+  if (getenv("UNSPLASH_KEY") == true){
     (new QuoteMaker)
-      ->setBackgroundFromUnsplash(["b353e61a07cc0068080258kc0294ks85042f2560d6223366500a2aa30ff28052"],"heart")
+      ->setBackgroundFromUnsplash([getenv("UNSPLASH_KEY"),"random"])
       ->quoteText($text)
       ->watermarkText("Mas Okky")
       ->toFile("result.jpg");
+  }
+  else{
+    (new QuoteMaker)
+      ->setBackground("yada.jpeg")
+      ->quoteText($text)
+      ->watermarkText("Mas Okky")
+      ->toFile("result.jpg");
+  }
 }catch(Exception $e){
   echo $e->getMessage();
 }
+$name = './result.jpg';
+$fp = fopen($name, 'rb');
+
+// send the right headers
+header("Content-Type: image/png");
+header("Content-Length: " . filesize($name));
+
+// dump the picture and stop the script
+fpassthru($fp);
+exit;
+?>
